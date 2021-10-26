29e74704d6cce0dea858ea56b5eb21979a4b0957
matteeyao <myaoski@gmail.com>
2021-10-25 21:50:50 -0500

    Reset program file to remove test cases

--- App/Program.cs
+++ App/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using App.Players;
 using App.UI;
 
@@ -9,13 +8,7 @@ namespace App
     {
         static void Main(string[] args)
         {
-            int numFields = (int) Math.Pow(3, 2);
-            // Console.Write(Enumerable.Range(1, numFields).Select(i => i.ToString()).ToArray().Equals(Enumerable.Range(1, numFields).Select(i => i.ToString()).ToArray()));
-            string[] arrayOne = Enumerable.Range(1, numFields).Select(i => i.ToString()).ToArray();
-            string[] arrayTwo = Enumerable.Range(1, numFields).Select(i => i.ToString()).ToArray();
-            Console.Write(new string[3]{"1", "2", "3"}.Equals(new string[3]{"1", "2", "3"}));
-
-            // CommandLine.Run();
+            CommandLine.Run();
         }
     }
 }
